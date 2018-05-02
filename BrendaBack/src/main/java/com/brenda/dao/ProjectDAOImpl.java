df99980e7fd8dd5424feb94c392af7c054e013ba
Aseza <abdelilahouchani3245@gmail.com>
2018-05-02 10:20:52 +0100

    Adding some Exception Handling samples

--- BrendaBack/src/main/java/com/brenda/dao/ProjectDAOImpl.java
+++ BrendaBack/src/main/java/com/brenda/dao/ProjectDAOImpl.java
@@ -15,6 +15,7 @@ import com.brenda.model.Project;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import javax.validation.constraints.Null;
 
 @Component
 @Repository
@@ -27,36 +28,57 @@ public class ProjectDAOImpl implements ProjectDAO {
 
 	@Override
 	public int removeProject(int id) {
-		Query query = getCurrentSession().createQuery("delete from Project where id=:ID");
-		query.setParameter("ID",id);
-		return query.executeUpdate();
+		Query query = null;
+		int result = -1;
+		try{
+		 	query = getCurrentSession().createQuery("delete from Project where id=:ID");
+			query.setParameter("ID",id);
+			result =  query.executeUpdate();
+		} catch (Exception e){
+			System.out.println("Can't create query, please check your entries");
+		}
+		return result;
 	}
 
 	@Override
 	public int createProject(Project project) {
-		Transaction tx1 = getCurrentSession().beginTransaction();
-		Serializable result = getCurrentSession().save(project);
-		tx1.commit();
-		return  (Integer) result;
+		Transaction tx1;
+		int result = -1;
+		try {
+			tx1= getCurrentSession().beginTransaction();
+			result = (Integer) getCurrentSession().save(project);
+			tx1.commit();
+		}catch (Exception e){
+			System.out.println("Can't get the Transaction, please check your entries");
+		}
+
+		return result;
 	}
 
 	@Override
 	public void updateProject(int id, Project newProject) {
-		Transaction tx2 = getCurrentSession().beginTransaction();
+		Transaction tx2;
 		Project oldProject = (Project) getCurrentSession().load(Project.class, id);
-		oldProject.setDeadLine(newProject.getDeadLine());
-		oldProject.setDescription(newProject.getDescription());
-		oldProject.setName(newProject.getName());
-		getCurrentSession().update(oldProject);
-		tx2.commit();
+		try{
+			tx2= getCurrentSession().beginTransaction();
+			oldProject.setDeadLine(newProject.getDeadLine());
+			oldProject.setDescription(newProject.getDescription());
+			oldProject.setName(newProject.getName());
+			getCurrentSession().update(oldProject);
+			tx2.commit();
+		} catch (SessionException e){
+			System.out.println("Session issue, Please Check Entries, Update Failed");
+		} catch (NullPointerException e){
+			System.out.println("The project object was not loaded/initialized successfully...Update Failed");
+		}
+
 
 	}
 
 	@Override
 	public Map<Integer, List<Project>> getProjectsLike(String word,int minIndex, int maxIndex) {
 		Query query = getCurrentSession().createQuery("From Project as pr where pr.name  like ?");
-			query.setString(0, "%" + word + "%");
-
+		query.setString(0, "%" + word + "%");
 		int size = query.list().size();
 		query.setFirstResult(minIndex);
 		query.setMaxResults(maxIndex);
